2bb56a91bea884eecdf3d59f83a403dfb8cfdad5
Rafael Melchioretto <melchiorettorafael@gmail.com>
2023-08-25 11:26:12 -0300

    Projeto Criado

---
+++ src/main/java/com/rafaelmelchioretto/gerenciaTarefas/GerenciaTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelmelchioretto.gerenciaTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelmelchioretto/gerenciaTarefas/GerenciaTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelmelchioretto.gerenciaTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
